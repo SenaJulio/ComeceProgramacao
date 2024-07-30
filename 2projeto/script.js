9017a439c4b9d9ff03bf913358975c7988154f96
JULIO SENA <gm.sena@hotmail.com>
2024-07-30 19:13:37 -0300

    eventos

---
+++ 2projeto/script.js
@@ -0,0 +1,8 @@
+function clicou() {
+    let botao = document.querySelector('.cta')
+    if (botao.classList.contains("secondary-color") === true) {
+       
+        botao.classList.add ='verde'
+    }
+   
+}
