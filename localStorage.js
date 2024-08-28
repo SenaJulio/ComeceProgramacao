24f58cd112e6958d569876d491ae48a3783594b5
JULIO SENA <gm.sena@hotmail.com>
2024-08-28 08:41:07 -0300

    projeto localStorage

--- localStorage.js
+++ localStorage.js
@@ -9,3 +9,27 @@ function ler() {
 function limpar(params) {
     localStorage.clear();
 }
+
+
+
+function setTheme(theme) {
+    localStorage.setItem('theme', theme);
+    updateTheme();
+}
+
+
+function updateTheme() {
+    let theme = localStorage.getItem('theme')
+document.querySelector('#theme').innerHTML = 'Tema: '+ theme;
+
+document.querySelector('body').classList.remove('light')
+document.querySelector('body').classList.remove('dark')
+
+if(theme === 'dark' ){
+    document.querySelector('body').classList.add('dark');
+   }else{
+    document.querySelector('body').classList.add('light');
+}
+   }
+updateTheme()
+
